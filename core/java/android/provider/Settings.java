4227fe2b46c5626236d800a78fa05e7c32dea004
Teoandro <teopesci@gmail.com>
2015-10-18 19:59:52 +0200

    Volume Rocker Wake [1/2]

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3481,7 +3481,13 @@ public final class Settings {
          */
         public static final String KEY_APP_SWITCH_DOUBLE_TAP_ACTION =
                 "key_app_switch_double_tap_action";
-
+                
+         /**   
+             * Volume rocker wake
+         * @hide
+         */
+        public static final String VOLUME_ROCKER_WAKE = "volume_rocker_wake";
+        
         /**
          * Action to perform when the camera key is pressed. (default: LAUNCH_CAMERA)
          * (See ButtonsConstants.java for valid values)
